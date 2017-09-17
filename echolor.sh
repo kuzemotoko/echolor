cc229271ac1f52eb84dc8f4cda9b009e398151f5
kuzemotoko <31139803+kuzemotoko@users.noreply.github.com>
2017-09-17 16:31:10 -0400

    Add files via upload

---
+++ echolor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo	Blue     : Directory
+
+	Green    : Executable or recognized data file
+
+	Sky Blue : Linked file
+
+	Yellow with black background: Device
+
+	Pink	 : Graphic image file		
+	
+	Red 	 : Archive file 
