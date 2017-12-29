8f3e71696c4f22aabe39cfbb18a166143cb3b5b3
kuzemotoko <31139803+kuzemotoko@users.noreply.github.com>
2017-12-28 18:58:39 -0800

    Update echolor.sh

--- echolor.sh
+++ echolor.sh
@@ -4,5 +4,5 @@ echo	Blue Directory
 echo  Green  Executable or recognized data file
 echo  Sky Blue  Linked file
 echo  Yellow with black background Device
-echo  Blue  Graphic image file		
+echo  Pink Graphic image file		
 echo  Red Archive file 
