8fec4d8e2dfd724f5e0c12f6e4f06d0f093c357a
kuzemotoko <31139803+kuzemotoko@users.noreply.github.com>
2018-04-16 03:12:07 +0000

    Update echolor.sh

--- echolor.sh
+++ echolor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#dont4gettheresaREADMEOK?
-echo  White TEXT FILE
-echo  Blue DIRECTORY
-echo  Green  EXECUTABLE OR RECOGNIZED DATA
-echo  Sky Blue  LINKED FILE
-echo  Yellow With Black Background DEVICE
-echo  Pink GRAPHIC IMAGE FILE
-echo  Red ARCHIVE FILE
-echo  Orange With Black Background BROKEN LINK
+echo  Executable files: Green
+echo  Directory: Blue
+echo  Image files(jpg, gif, bmp, png, tif): Magenta
+echo  Symbolic links: Cyan
+echo  Pipe: Yellow
+echo  Socket: Magenta
+echo  Orphaned symbolic links: Blinking Bold white with red background
+echo  Block device driver: Bold yellow foreground, with black background
+echo  Missing links along with files they point to: Blinking Bold white with red background
+echo  Archives or compressed files(like tar,gz,zip,rpm): Red
