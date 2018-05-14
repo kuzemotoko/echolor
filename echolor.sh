648f45b787b9a764090387144db3ac9a724497a0
kuzemotoko <31139803+kuzemotoko@users.noreply.github.com>
2018-05-14 07:44:30 -0700

    Update echolor.sh

--- echolor.sh
+++ echolor.sh
@@ -2,12 +2,12 @@
 echo  Text files : White
 echo  Executable files: Green
 echo  Directory: Blue
-echo  Image files(jpg, gif, bmp, png, tif): Magenta
+echo  Image filesjpg, gif, bmp, png, tif: Magenta
 echo  Symbolic links: Cyan
 echo  Pipe: Yellow
 echo  Socket: Magenta
 echo  Orphaned symbolic links: Blinking Bold white with red background
 echo  Block device driver: Bold yellow foreground, with black background
 echo  Missing links along with files they point to: Blinking Bold white with red background
-echo  Archives or compressed files(like tar,gz,zip,rpm): Red
+echo  Archives or compressed files like Tar,gz,zip,rpm : Red
 
